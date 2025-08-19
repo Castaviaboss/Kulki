0b167f9323b78dc43dcb916e56ab3a4690840378
Castaviaboss <102110038+Castaviaboss@users.noreply.github.com>
2025-08-19 20:37:49 +0200

    Initial commit

---
+++ Source/Kulki/Kulki.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Kulki.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Kulki, "Kulki" );
